9d2a44cc88a2754262f39353980552d017c02abe
alex <yaobo-1986@163.com>
2017-05-12 14:58:32 +0800

    习题3：数字和数学计算

--- Aqrun/ex03.py
+++ Aqrun/ex03.py
@@ -5,8 +5,8 @@
 
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 /6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens %s" % (25 + 30 / 6))
+print("Roosters %s" % (100 - 25 * 3 % 4))
 
 print("Now I will count the eggs:")
 
@@ -14,15 +14,15 @@ print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
-print( 3 + 2 < 5 - 7)
+print(3 + 2 < 5 - 7)
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print("What is 3 + 2? %s" % (3 + 2))
+print("What is 5 - 7? %s" % (5 - 7))
 
 print("Oh, that's why it's False.")
 
 print("How About some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater? %s" % (5 > -2))
+print("Is it greater or equal? %s" % (5 >= -2))
+print("Is it less or equal? %s" % (5 <= -2))
