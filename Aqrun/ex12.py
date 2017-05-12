641892227e82a4cc420b56c1ad59071f79eee2f3
alex <yaobo-1986@163.com>
2017-05-12 16:17:37 +0800

    习题 12: 提示别人

---
+++ Aqrun/ex12.py
@@ -0,0 +1,12 @@
+# -- coding: utf-8 --
+"""
+习题 12: 提示别人
+"""
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+
+print("So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight
+))
