8338a0ca73c2656b916ca5884bd510a34e716441
alex <yaobo-1986@163.com>
2017-05-12 17:19:02 +0800

    习题 15: 读取文件

---
+++ Aqrun/ex15.py
@@ -0,0 +1,29 @@
+# -- coding:utf-8 --
+"""
+习题 15: 读取文件
+"""
+
+from sys import argv
+
+def main_func():
+    data = (0, 0)
+    if len(argv) < 2:
+        exit('ERROR: arg filename is required!')
+    else:
+        data = argv
+    script, filename = data
+
+    txt = open(filename)
+    print("Here's your file %r:" % filename)
+    print(txt.read())
+    txt.close()
+
+    print "Type the filename again:"
+    file_again = raw_input(">")
+    txt_again = open(file_again)
+    print(txt_again.read())
+    txt_again.close()
+
+if __name__=="__main__":
+    main_func()
+
