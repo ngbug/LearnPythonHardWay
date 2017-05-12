cd7cc3a414ebe296f258db25cd39facf127239f4
alex <yaobo-1986@163.com>
2017-05-12 15:01:40 +0800

    习题2：注释和井号

--- Aqrun/ex02.py
+++ Aqrun/ex02.py
@@ -8,4 +8,7 @@
 
 print("I could have code like this.") #后面的注释会被忽略
 
-# 你也可以使用
+# You can also use a comment to "disable" or comment out a piece of code:
+# print("This won't run.")
+
+print "This will run."
