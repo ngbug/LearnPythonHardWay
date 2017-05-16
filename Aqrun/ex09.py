d18b523a7a38fe266b62ad6a497588803108a86e
alex306314 <yaobo-1986@163.com>
2017-05-16 22:31:41 +0800

    习题9: 打印，打印，打印

--- Aqrun/ex09.py
+++ Aqrun/ex09.py
@@ -11,9 +11,9 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 print("Here are the days: %s" % days)
 print("Here are the months: %s" % months )
 
-print """
+print("""
 There's something going here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
-"""
+""")
