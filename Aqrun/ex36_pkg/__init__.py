1b18cf22654458b0eeb48026d3ef9e15fe326c6b
alex306314 <yaobo-1986@163.com>
2017-05-16 23:26:20 +0800

    完成剪刀石头布游戏

--- Aqrun/ex36_pkg/__init__.py
+++ Aqrun/ex36_pkg/__init__.py
@@ -4,7 +4,7 @@
 游戏设计：
 游戏大厅显示游戏列表：
 石头剪刀布
-杠子 老虎 鸡
+杠子老虎鸡
 """
 
 from rock_paper_scissors import start as rock_start
@@ -13,29 +13,38 @@ from stick_tiger_chicken import start as stick_start
 def show_welcome_menu():
     """显示大厅欢迎列表"""
     print("""
-    ============================
-    #                          #
-    # 欢迎来到 *了扎咧* 游戏大厅   #
-    #                          #
-    #     1. 石头剪刀布          #
-    #     2. 杠子 老虎 鸡        #
-    #                          #
-    ============================
+    ==================================================
+    #                                                #
+                欢迎来到 *了扎咧* 游戏大厅          
+                                                    
+                   1. 石头剪刀布                    
+                   2. 杠子老虎鸡
+                
+                   0. 退出游戏大厅
+    #                                                #
+    ==================================================
     """)
 
 
 def main_hall():
-    "游戏大厅 所有游戏列表"
-    show_welcome_menu()
-    
+    """游戏大厅 所有游戏列表"""
+    next = -1
 
-    next = raw_input('你想玩什么游戏? > ')
-    
-    if next == '1':
-        rock_start()
-        pass
-    elif next == '2':
-        stick_start()
-        pass
-    else:
-        print("客官，不可以哦～～")
+    while True:
+        show_welcome_menu()
+
+        #python3.0 raw_input rename to input
+        next = input('你想玩什么游戏? > ')
+
+        if next == '0':
+            print("欢迎再来哦~~")
+            break
+        elif next == '1':
+            rock_start()
+        elif next == '2':
+            stick_start()
+        else:
+            print("客官，不可以哦～～")
+
+if __name__ == '__main__':
+    main_hall()
--- Aqrun/ex36_pkg/rock_paper_scissors.py
+++ Aqrun/ex36_pkg/rock_paper_scissors.py
@@ -1,9 +1,97 @@
 # -- coding: utf-8 --
 """
-游戏剪刀石头布
+游戏 剪刀石头布
 """
+
+from random import randrange
+
+
+class RockPaperScissors(object):
+    """Game class"""
+
+    hands = {
+        1 : '石头',
+        2 : '剪刀',
+        3 : '布',
+    }
+
+    success_list = [(1, 2), (2, 3), (3, 1)]
+
+    def __init__(self):
+        # 保存电脑的手形
+        self.robot_hand = 1
+
+    def generateRobotHand(self):
+        self.robot_hand = randrange(1, 4)
+
+    def getUserHand(self):
+        print("""
+        ================
+            1. 石头
+            2. 剪刀 
+            3. 布
+            
+            0 返回游戏大厅
+        ================
+        """)
+        user_hand = input("出手吧 > ")
+        return user_hand
+
+    def showResult(self, user_hand, robot_hand):
+        """Show game result"""
+        result_set = (user_hand, robot_hand)
+
+        if user_hand == robot_hand:
+            template_string = """
+
+            %s  =>  %s
+            好尴尬啊~~ 出成一样的了。再来！！
+
+            """
+        elif result_set in self.success_list:
+            template_string = """
+            
+            %s  =>  %s
+            恭喜你！ 你赢了~~
+            
+            """
+        else:
+            template_string = """
+            
+            %s  =>  %s
+            唉呀！不好意思你输了。再接再励吧~！
+            
+            """
+
+        print(template_string % (self.hands[user_hand],
+                   self.hands[robot_hand]))
+        input('点击任意键继续...')
+        self.run()
+
+    def run(self):
+        """Main loop"""
+        self.generateRobotHand()
+        user_hand = 0
+
+        try:
+            user_hand = eval(self.getUserHand())
+        except:
+            print("你是逗B吗？请不要乱输入...")
+            self.run()
+
+        if user_hand == 0:
+            print("欢迎再来~~")
+        elif user_hand >=1 and user_hand <=3:
+            self.showResult(user_hand, self.robot_hand)
+        else:
+            print("你是逗B吗？请不要乱输入...")
+            self.run()
 
 def start():
     """start"""
-    pass
+    game = RockPaperScissors()
+    game.run()
+
+if __name__ == "__main__":
+    start()
 
