b85a865ced7a4763d07fcb94ccba2a4f69c951c6
alex306314 <yaobo-1986@163.com>
2017-05-16 22:19:08 +0800

    习题 15: 读取文件

--- Aqrun/ex15.py
+++ Aqrun/ex15.py
@@ -18,8 +18,8 @@ def main_func():
     print(txt.read())
     txt.close()
 
-    print "Type the filename again:"
-    file_again = raw_input(">")
+    print("Type the filename again:")
+    file_again = input(">")
     txt_again = open(file_again)
     print(txt_again.read())
     txt_again.close()
