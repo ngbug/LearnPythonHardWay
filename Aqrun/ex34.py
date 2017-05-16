a42f74d572a778b6f6024244202ca9686b112846
Aqrun <meanofwind@gmail.com>
2017-05-16 09:39:46 +0800

    习题 34: 访问列表的元素

--- Aqrun/ex34.py
+++ Aqrun/ex34.py
@@ -3,4 +3,19 @@
 习题 34: 访问列表的元素
 """
 
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale',
+'platypus']
+
+print("The 2nd animal is at 1 and is a python -> %s" % 
+animals[1])
+print("The 3rd animal is at 2 and is a peacock -> %s" % 
+animals[2])
+print("The 1st animal is at 0 and is a bear -> %s" % 
+animals[0])
+print("The 4th animal is at 3 and is a kangaroo -> %s" % 
+animals[3])
+print("The 5th animal is at 4 and is a whale -> %s" % 
+animals[4])
+print("The 6th animal is at 5 and is a playtypus -> %s" % 
+animals[5])
 
