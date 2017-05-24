ffdb1c12e53c63f248f284b351a7e7c46b99afda
alex306314 <yaobo-1986@163.com>
2017-05-24 20:36:27 +0800

    习题 14: 提示和传递

--- Aqrun/ex14.py
+++ Aqrun/ex14.py
@@ -18,19 +18,19 @@ def main_func():
     print("Hi %s, I'm the %s script." % (user_name, script))
     print("I'd like to ask you a few questions.")
     print("Do you like me %s?" % user_name)
-    likes = raw_input(prompt)
+    likes = input(prompt)
 
     print("Where do you live %s?" % user_name)
-    lives = raw_input(prompt)
+    lives = input(prompt)
 
     print("What kind of computer do you have?")
-    computer = raw_input(prompt)
+    computer = input(prompt)
 
-    print """
+    print ("""
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is. 
 And you have a %r computer. Nice.
-    """ % (likes, lives, computer)
+    """ % (likes, lives, computer))
 
 if __name__ == '__main__':
     main_func()
