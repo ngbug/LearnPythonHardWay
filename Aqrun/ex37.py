a4377800d0bebbba15054a68388762966ad4433f
Aqrun <meanofwind@gmail.com>
2017-05-26 18:38:55 +0800

    习题 37: 复习各种符号

---
+++ Aqrun/ex37.py
@@ -0,0 +1,115 @@
+# -- coding: utf-8 --
+"""
+习题 37: 复习各种符号
+
+
+## Keywords
+
+* and
+* del
+* from 
+* not
+* while
+* as 
+* elif
+* global
+* or
+* with
+* assert
+* else
+* if
+* pass
+* yield
+* break
+* except
+* import
+* print
+* class
+* exec
+* in 
+* raise
+* continue
+* finally
+* is
+* return
+* def
+* for
+* lambda
+* try
+
+## 数据类型
+
+* True
+* False
+* None
+* strings
+* numbers
+* floats
+* lists
+
+## 字符串转义序列
+
+* \\
+* \'
+* \"
+* \a
+* \b
+* \f
+* \n
+* \r
+* \t
+* \v
+
+## 字符串格式化
+
+* %d
+* %i
+* %o
+* %u
+* %x
+* %X
+* %e
+* %f
+* %F
+* %g
+* %G
+* %c
+* %r
+* %s
+* %%
+
+## 操作符号
+
+* + 
+* - 
+* * 
+* **
+* /
+* //
+* %
+* <
+* >
+* <=
+* >=
+* ==
+* !=
+* <>
+* ()
+* []
+* {}
+* @
+* ,
+* :
+* .
+* =
+* ;
+* +=
+* -=
+* *=
+* /=
+* //=
+* %=
+* **=
+
+"""
+
